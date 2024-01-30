51f4108780e1e917f5083320e1aeb0f1aa01313f
Dannyorji <dnnyorji@gmail.com>
2024-01-30 08:36:56 +0100

    create a private route and custom hook for profile protection

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "Daniel",
+    email: "dnnyorji@gmail.com",
+  });
+  const { name, email } = formData;
+  function handleOnClick() {
+    auth.signOut();
+    navigate("/");
+  }
+  return (
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold ">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            {/*   */}
+
+            <input
+              type="text"
+              value={name}
+              name="name"
+              disabled
+              className="w-full px-4 py-2 text-gray-700 text-xl bg-white border border-gray-300 rounded mb-6"
+            />
+            {/*   */}
+            <input
+              type="email"
+              value={email}
+              name="email"
+              disabled
+              className="w-full px-4 py-2 text-gray-700 text-xl bg-white border border-gray-300 rounded mb-6"
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="flex items-center ">
+                Do you want to change your name?
+                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  Edit
+                </span>
+              </p>
+              <p
+                onClick={handleOnClick}
+                className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-150 cursor-pointer"
+              >
+                Sign Out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
